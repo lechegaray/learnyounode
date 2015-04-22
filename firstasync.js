18bdc7d80c41afd00de997d9b227064f3300896b
lechegaray <mrluisechegaray@gmail.com>
2015-04-22 11:57:39 -0400

    next problem solved

---
+++ firstasync.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+
+	fs.readFile(process.argv[2], function finishedReading(err, data) {
+		if (err) return console.error(err);
+		var lines = data.toString().split('\n').length-1;
+		console.log(lines);
+
+	})
+
+
+    // var fs = require('fs')
+    // var file = process.argv[2]
+    
+    // fs.readFile(file, function (err, contents) {
+    //   // fs.readFile(file, 'utf8', callback) can also be used
+    //   var lines = contents.toString().split('\n').length - 1
+    //   console.log(lines)
+    // })
